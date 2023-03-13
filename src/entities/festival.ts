cc8ee9fa5e21d881207153f0f3aadec717c5aba6
Ariadna Frias <ariadna.812@gmail.com>
2023-03-13 18:17:23 +0100

    Add festival entity

---
+++ src/entities/festival.ts
@@ -0,0 +1,13 @@
+import { User } from './user';
+
+export type Festival = {
+  id: string;
+  name: string;
+  image: string;
+  musicType: string;
+  city: string;
+  country: string;
+  dates: string;
+  capacity: number;
+  owner: User;
+};
