3347dae4f14edd711808a07844057c57fa4b39bf
Ariadna Frias <ariadna.812@gmail.com>
2023-03-13 20:57:14 +0100

    Add users router ts file

---
+++ src/router/users.router.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { UsersController } from '../controllers/users.controller.js';
+import { logged } from '../interceptors/logged.js';
+import { FestivalsMongoRepo } from '../repository/festivals.mongo.repo.js';
+import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
+
+// eslint-disable-next-line new-cap
+export const usersRouter = Router();
+
+const repoUsers = new UsersMongoRepo();
+const repoFestivals = new FestivalsMongoRepo();
+const userController = new UsersController(repoUsers, repoFestivals);
+
+usersRouter.get('/', logged, userController.getAll.bind(userController));
+usersRouter.post('/register', userController.register.bind(userController));
+usersRouter.post('/login', userController.login.bind(userController));
+usersRouter.patch(
+  '/addfav/:id',
+  logged,
+  userController.addFavorite.bind(userController)
+);
+usersRouter.patch(
+  '/deletefav/:id',
+  logged,
+  userController.deleteFavorite.bind(userController)
+);
