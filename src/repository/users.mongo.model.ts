7a11bda43369b27deef7c84f00df51346c2b489b
Ariadna Frias <ariadna.812@gmail.com>
2023-03-13 20:06:03 +0100

    Add user mongo model

---
+++ src/repository/users.mongo.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+import { User } from '../entities/user';
+
+const userSchema = new Schema<User>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  favoriteFestival: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Festival',
+    },
+  ],
+});
+
+userSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject._id;
+    delete returnedObject.password;
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');
