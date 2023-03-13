15b62f482112b794c5575a34f4e0bab2c4ab23e4
Ariadna Frias <ariadna.812@gmail.com>
2023-03-13 16:01:34 +0100

    Add index ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-const x = 22;
+/* eslint-disable spaced-comment */
+import createDebug from 'debug';
+import http from 'http';
+import { app } from './app.js';
+import { dbConnect } from './db/db.connect.js';
+
+const debug = createDebug('FINPR');
+const PORT = process.env.PORT || 4500;
+
+const server = http.createServer(app);
+
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    debug('DB:', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => server.emit('error', error));
+
+server.on('error', (error) => {
+  debug('Server error:', error.message);
+});
+
+server.on('listening', () => {
+  debug('Listening in http://localhost:' + PORT);
+});
