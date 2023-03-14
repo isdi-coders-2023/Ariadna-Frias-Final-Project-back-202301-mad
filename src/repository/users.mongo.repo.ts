aa0663c08f599515edcbb195c973379d9c77db95
Ariadna Frias <ariadna.812@gmail.com>
2023-03-14 16:33:08 +0100

    Update users mongo repo

--- src/repository/users.mongo.repo.ts
+++ src/repository/users.mongo.repo.ts
@@ -31,9 +31,7 @@ export class UsersMongoRepo implements UserRepo<User> {
 
   async search(query: { key: string; value: unknown }) {
     debug('search method');
-    const data = await UserModel.find({ [query.key]: query.value }).populate(
-      'favoriteFestival'
-    );
+    const data = await UserModel.find({ [query.key]: query.value });
     return data;
   }
 
