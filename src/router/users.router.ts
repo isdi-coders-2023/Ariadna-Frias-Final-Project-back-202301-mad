5d9076bdd32e70404a57345031b8e13be6b4a6fe
Ariadna Frias <ariadna.812@gmail.com>
2023-03-14 18:57:53 +0100

    Update users router

--- src/router/users.router.ts
+++ src/router/users.router.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { UsersController } from '../controllers/users.controller.js';
-import { logged } from '../interceptors/logged.js';
 import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
 
 // eslint-disable-next-line new-cap
@@ -10,6 +9,6 @@ const repoUsers = new UsersMongoRepo();
 
 const userController = new UsersController(repoUsers);
 
-usersRouter.get('/', logged, userController.getAll.bind(userController));
+usersRouter.get('/', userController.getAll.bind(userController));
 usersRouter.post('/register', userController.register.bind(userController));
 usersRouter.post('/login', userController.login.bind(userController));
